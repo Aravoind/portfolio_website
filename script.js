a2e739e4d07e2e3adb47987e8f655bbf54ac4ddd
Aravind S <161047197+Aravoind@users.noreply.github.com>
2026-02-02 13:22:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed =new Typed(".typing",{
+    strings:["", "Data Analyst","","Web Developer","","Data Analyst","","Web Developer",""],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+})
